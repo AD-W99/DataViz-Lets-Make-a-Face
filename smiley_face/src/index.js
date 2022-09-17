274312103956579a1bcae287f4c4f12024f14bae
Alexander <alex.wood1722@gmail.com>
2022-09-16 20:08:43 -0700

    Created two new components

--- smiley_face/src/index.js
+++ smiley_face/src/index.js
@@ -1,38 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BackgroundCircle } from './BackgroundCircle'
-import { Eyes } from './Eyes'
-import { Mouth } from './Mouth'
+import { Face } from './Face'
 
 const width = 960 
 const height = 500
-const centerX = width / 2
-const centerY = height / 2
-const strokeWidth = 20
-const eyeOffsetX = 90
-const eyeOffsetY = 100
-const eyeRadius = 40
-const mouthWidth = 20
-const mouthRadius = 140
+
 
 const App = () => (
-	<svg width={width} height={height}>
-    <g transform={`translate(${centerX}, ${centerY})`}>
-        <BackgroundCircle
-          radius={centerY - strokeWidth / 2}
-          strokeWidth={strokeWidth}
-        />
-        <Eyes
-          eyeOffsetX={eyeOffsetX}
-          eyeOffsetY={eyeOffsetY}
-          eyeRadius={eyeRadius}
-        />
-        <Mouth
-          mouthRadius={mouthRadius}
-          mouthWidth={mouthWidth}
-        />
-    </g>
-  </svg>
+	<Face 
+    width={width}
+    height={height}
+  	centerX={width / 2}
+		centerY={height / 2}
+		strokeWidth={20}
+		eyeOffsetX={90}
+		eyeOffsetY={100}
+		eyeRadius={40}
+		mouthWidth={20}
+		mouthRadius={140}  
+  />
 )
 
 const rootElement = document.getElementById('root')
